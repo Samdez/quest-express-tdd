e5d70fa82375b4c1b35025fc0beaaceabe678dfa
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2021-01-02 21:59:21 +0100

    test 3

--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -1,7 +1,10 @@
 const request = require('supertest')
 const app = require('../app')
+const connection = require('../connection')
 
 describe('Test routes', () => {
+  beforeEach(done => connection.query('TRUNCATE bookmark', done))
+
   it('GET / sends "Hello World" as json', (done) => {
     request(app)
       .get('/')
@@ -13,4 +16,51 @@ describe('Test routes', () => {
         done();
       })
   })
+
+  it('POST /bookmarks error(field missing)', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({})
+      .expect(422)
+      .then(response => {
+        const expected = { "error": "required field(s) missing"}
+        expect(response.body).toEqual(expected);
+        done();
+      })
+  })
+
+  it('POST /bookmarks - OK (fields provided) ', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({ url: 'https://jestjs.io', title: 'Jest' })
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = { id: 1, url: 'https://jestjs.io', title: 'Jest' };
+        expect(response.body).toEqual(expected);
+        done();
+      })
+      .catch(done);
+  });
+
+  // describe('GET /bookmarks/:id', () => {
+  //   const testBookmark = { url: 'https://nodejs.org/', title: 'Node.js' };
+  //   beforeEach((done) => connection.query(
+  //     'TRUNCATE bookmark', () => connection.query(
+  //       'INSERT INTO bookmark SET ?', testBookmark, done
+  //     )
+  //   ));
+
+  //   it('GET /bookmark/:id error(id not found)', (done) => {
+  //     request(app)
+  //       .get('/bookmark/:id')
+  //       .expect(404)
+  //       .expect('Content-Type', /json/)
+  //       .then(response => {
+  //         const expected = { error: 'Bookmark not found'}
+  //         expect(response.body).toEqual(expected);
+  //         done();
+  //       })
+  //   })
+  // })
 })
